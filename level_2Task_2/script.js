7714c6318cced6395c5e34cb480365ccdbd549b2
missesenam <moneyesenam@gmail.com>
2024-10-10 19:46:23 +0100

    task 2 done

---
+++ level_2Task_2/script.js
@@ -0,0 +1,8 @@
+        // Scroll Indicator Script
+        window.onscroll = function() { scrollProgress() };
+        function scrollProgress() {
+            var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+            var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+            var scrolled = (winScroll / height) * 100;
+            document.getElementById("progressBarFill").style.width = scrolled + "%";
+        }
